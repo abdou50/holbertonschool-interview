4f75b065d55c01915c15b949189f1a9f593de541
Abdelkader Raddadi <gadourraddadi@gmail.com>
2022-12-08 18:43:55 +0100

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,6 +5,8 @@
 def canUnlockAll(boxes):
     t = [0]
     for j, i in enumerate(boxes):
+        if not i:
+            return False
         for keys in i:
             if keys != j and keys not in t:
                 t.append(keys)
