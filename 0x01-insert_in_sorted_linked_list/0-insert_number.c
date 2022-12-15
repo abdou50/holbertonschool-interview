a21e40c85691015d338a6219af17bb9d55430b22
Abdelkader Raddadi <gadourraddadi@gmail.com>
2022-12-15 15:29:23 +0100

    l

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current;
+
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL || (*head)->n >= new_node->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		current = *head 
+		while (current->next != NULL && current->next->n < new_node->n)
+		{
+			current = current->next;
+		}
+
+		new_node->next = current->next;
+		current->next = new_node;
+	}
+	return (new_node);
+}
