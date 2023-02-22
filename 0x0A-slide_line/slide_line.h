1ae0c1d328a21dd876704fa022766b4b846c1d01
abdou50 <gadourraddadi@gmail.com>
2023-02-22 21:26:42 +0100

    l

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef _SLIDE_LINE8H
+#define _SLIDE_LINE_H
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 0
+#include <stdio.h>
+#include <stdlib.h>
+int slide_line(int *line, size_t size, int direction);
+void pushr(int *arr, size_t n);
+void pushl(int *arr, size_t n)
+void merger(int *list, size_t size);
+void mergel(int *list, size_t size)
+#endif
