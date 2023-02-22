c0b73197a57a37d680b5cb547c08399d63e0b380
abdou50 <gadourraddadi@gmail.com>
2023-02-22 22:01:44 +0100

    l

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,5 +1,10 @@
 #include "slide_line.h"
-
+/**
+ * pushr - Prints out an array of integer, followed by a new line
+ *
+ * @arr: Pointer to the array of integer to be printed
+ * @n: Number of elements in @array
+ */
 void pushr(int *arr, size_t n)
 {
 	size_t i, j;
@@ -14,10 +19,14 @@ void pushr(int *arr, size_t n)
 				arr[j + 1] = aux;
 			}
 }
-
+/**
+ * merger - Prints out an array of integer, followed by a new line
+ * @array: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ */
 void merger(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
 	for (i = size - 1; i + 1 > 1; i--)
 	{
@@ -29,6 +38,12 @@ void merger(int *array, size_t size)
 		}
 	}
 }
+/**
+ * pushl - Prints out an array of integer, followed by a new line
+ *
+ * @arr: Pointer to the array of integer to be printed
+ * @n: Number of elements in @array
+ */
 void pushl(int *arr, size_t n)
 {
 	size_t i, j;
@@ -43,9 +58,15 @@ void pushl(int *arr, size_t n)
 				arr[j + 1] = aux;
 			}
 }
+/**
+ * mergel - Prints out an array of integer, followed by a new line
+ *
+ * @array: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ */
 void mergel(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -57,9 +78,16 @@ void mergel(int *array, size_t size)
 		}
 	}
 }
+/**
+ * slide_line - Prints out an array of integer, followed by a new line
+ * @line: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ * @direction: R or l
+ * Return: 1 or 0
+ */
 int slide_line(int *line, size_t size, int direction)
 {
-	if (line == NULL || size <1)
+	if (line == NULL || size < 1)
 		return (0);
 
 	if (direction == SLIDE_LEFT)
