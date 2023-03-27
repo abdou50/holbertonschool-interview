c53a85d739f6c261b4b51fcc29ca95edc61bb752
abdou50 <gadourraddadi@gmail.com>
2023-03-27 17:49:55 +0100

    l

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,42 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
+
+/**
+ * createavlnode - create a node
+ * @n: value of node
+ * @parent:parent node
+ * Return: the node d
+ */
 avl_t *createavlnode(int n, avl_t *parent)
 {
-  avl_t *node = malloc(sizeof(*node));
-  node->n = n;
-  node->left = NULL;
-  node->right = NULL;
-  node->parent = parent;
-  return (node);
+	avl_t *node = malloc(sizeof(*node));
+
+	if (!new)
+		return (NULL)node
+	node->n = n;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
 
 /**
- * print_t - Stores recursively each level in an array of strings
- *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
+ * recursively - search where to add in tree
+ * @parent: parent node
+ * @array: array of values
+ * @begin: begin of the array
+ * @sizearray: size of the array
+ * Return: null or avl
  */
-
-avl_t *recursively(int *array, int begin, int sizearray, avl_t *parent)
+avl_t *recursively(avl_t *parent, int *array, int begin, int sizearray)
 {
-    avl_t *node;
-    int i;
+	avl_t *new;
+	int i;
 
-    i = (begin + sizearray) / 2;
-    node = createavlnode(array[i], parent);
-    if (!node)
-        return (NULL);
-    node->right = recursively(array, i + 1 , sizearray - 1, node);
-    node->left = recursively(array, begin, i - 1 ,node);
-    return(node);
+	if (begin > sizearray)
+		return (NULL);
+	i = (begin + sizearray) / 2;
+	new = createavlnode(array[i], parent);
+	if (!new)
+		return (NULL);
+	new->left = recursively(new, array, begin, i - 1);
+	new->right = recursively(new, array, i + 1, sizearray);
+	return (new);
 }
+
+/**
+ * sorted_array_to_avl - build AVL tree from array
+ * @array: pointer to first element of array
+ * @size: size of array
+ *
+ * Return:  root or null
+ */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    return(recursively(array, 0, size, NULL));
+	return (recursively(NULL, array, 0, size - 1));
 }
