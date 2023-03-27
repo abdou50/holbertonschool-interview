1c5295f759a38e17883c615dad022255b5a3b3f0
abdou50 <gadourraddadi@gmail.com>
2023-03-27 16:53:07 +0100

    l

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,1 +1,42 @@
-0-sorted_array_to_avl.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+avl_t *createavlnode(int n, avl_t *parent)
+{
+  avl_t *node = malloc(sizeof(*node));
+  node->n = n;
+  node->left = NULL;
+  node->right = NULL;
+  node->parent = parent;
+  return (node);
+}
+
+/**
+ * print_t - Stores recursively each level in an array of strings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
+ */
+
+avl_t *recursively(int *array, int begin, int sizearray, avl_t *parent)
+{
+    avl_t *node;
+    int i;
+
+    i = (begin + sizearray) / 2;
+    node = createavlnode(array[i], parent);
+    if (!node)
+        return (NULL);
+    node->right = recursively(array, i + 1 , sizearray - 1, node);
+    node->left = recursively(array, begin, i - 1 ,node);
+    return(node);
+}
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    return(recursively(array, 0, size, NULL));
+}
