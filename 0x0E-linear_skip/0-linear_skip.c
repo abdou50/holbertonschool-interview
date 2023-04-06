53b7e83fa9200583894cd2042fe93576d0aa17b5
abdou50 <gadourraddadi@gmail.com>
2023-04-06 23:31:55 +0100

    l

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -5,43 +5,43 @@
  * @list: Pointer to the head of the skip list to search in
  * @value: Value to search for
  *
- * Return: Pointer on the first node where value is located or NULL if not found
+ * Return: Pointer on the first node or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express = list, *curr = list;
+	skiplist_t *express = list, *curr = list;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    while (express && express->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n",
-               express->index, express->n);
-        curr = express;
-        express = express->express;
-    }
+	while (express && express->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				express->index, express->n);
+		curr = express;
+		express = express->express;
+	}
 
-    if (express)
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               curr->index, express->index);
-    else
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               curr->index, curr->index);
+	if (express)
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				curr->index, express->index);
+	else
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				curr->index, curr->index);
 
-    while (curr && curr->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n",
-               curr->index, curr->n);
-        curr = curr->next;
-    }
+	while (curr && curr->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+		curr = curr->next;
+	}
 
-    if (curr && curr->n == value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n",
-               curr->index, curr->n);
-        return (curr);
-    }
+	if (curr && curr->n == value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+		return (curr);
+	}
 
-    return (NULL);
+	return (NULL);
 }
