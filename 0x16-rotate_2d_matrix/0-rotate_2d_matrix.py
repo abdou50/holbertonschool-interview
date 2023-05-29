4311502f5f22a19a7a7d5529605de2a3c92425db
abdou50 <gadourraddadi@gmail.com>
2023-05-29 12:27:24 +0100

    l

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Test 0x16 - Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """matrix rotate"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = matrix[i][j]
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
