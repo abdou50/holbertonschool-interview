74a7e2c02abed5d3a675313293a0aeabebf531d2
abdou50 <gadourraddadi@gmail.com>
2023-06-09 00:14:56 +0100

    fix

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
 #include "search_algos.h"
 
+
+/**
+ *print_array -print array of
+ *@array: array
+ *@start: is the first
+ *@end: is the last
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		if (i == end)
+		{
+			printf("%d", array[i]);
+		}
+		else
+		{
+			printf("%d, ", array[i]);
+		}
+	}
+	printf("\n");
+}
+/**
+ * search_into_array - function that print array of
+ *@array:  is a pointer to the first element of the array to search in
+ *@start: is the first index to print array
+ *@end: the end index to print array
+ *@value: is the last index to print array
+ * Return: index of value
+ */
+int search_into_array(int *array, size_t start, size_t end, int value)
+{
+	size_t mid_value;
+
+	if (start <= end)
+	{
+		print_array(array, start, end);
+		mid_value = (start + end) / 2;
+		if (array[mid_value] == value && array[mid_value - 1] != value)
+		{
+			return (mid_value);
+		}
+		else if (array[mid_value] < value)
+		{
+			return (search_into_array(array, mid_value + 1, end, value));
+		}
+		else
+		{
+			return (search_into_array(array, start, mid_value, value));
+		}
+	}
+	return (-1);
+}
+
 /**
  * advanced_binary - function
  * @array: the array
@@ -10,45 +67,9 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return (-1);
-
-	return (binary_search(array, 0, size - 1, value));
-}
-/**
- * binary_search - function
- * @array: the array
- * @start: the start
- * @end: the end
- * @value: the value
- * Return: the position or -1
- */
-int binary_search(int *array, int start, int end, int value)
-{
-	int mid, i;
-
-	if (start > end)
-		return (-1);
-
-	mid = start + (end - start) / 2;
-	printf("Searching in array: ");
-	for (i = start; i <= end; i++)
+	if (!array)
 	{
-		printf("%d", array[i]);
-		if (i < end)
-			printf(", ");
+		return (-1);
 	}
-	printf("\n");
-
-	if (array[mid] == value)
-	{
-		if (mid == 0 || array[mid - 1] != value)
-			return (mid);
-		else
-			return (binary_search(array, start, mid - 1, value));
-	}
-	else if (array[mid] < value)
-		return (binary_search(array, mid + 1, end, value));
-	else
-		return (binary_search(array, start, mid - 1, value));
+	return (search_into_array(array, 0, size - 1, value));
 }
