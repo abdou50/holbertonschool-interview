3c9270d5387ce19b69529889da9d9edc97614e88
abdou50 <gadourraddadi@gmail.com>
2023-06-09 03:04:07 +0100

    l

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,45 +2,49 @@
 import sys
 
 
-def is_valid(board, row, col, N):
-    for i in range(N):
-        if board[row][i] == 1 or board[i][col] == 1:
+def queen_pos(board, pos):
+    """function"""
+
+    for k in board:
+        if k[1] == pos[1]:
             return False
-    for i in range(N):
-        for j in range(N):
-            if (i+j) == (row+col) or (i-j) == (row-col):
-                if board[i][j] == 1:
-                    return False
+        if (k[0] + k[1]) == (pos[0] + pos[1]):
+            return False
+        if (k[0] - k[1]) == (pos[0] - pos[1]):
+            return False
     return True
 
 
-def n_queens(board, col, N):
-    if col == N:
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == 1:
-                    print(j+1, end=' ')
-        print()
-        return True
-    res = False
-    for i in range(N):
-        if is_valid(board, i, col, N):
-            board[i][col] = 1
-            res = n_queens(board, col+1, N) or res
-            board[i][col] = 0
-    return res
+def place_queens(x, n, board):
+    """function"""
+
+    if x == n:
+        print(board)
+    else:
+        for y in range(n):
+            pos = [x, y]
+            if queen_pos(board, pos):
+                board.append(pos)
+                place_queens(x + 1, n, board)
+                board.remove(pos)
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if __name__ == '__main__':
+    """function"""
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    n = int(sys.argv[1])
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = []
+    place_queens(0, n, board)
