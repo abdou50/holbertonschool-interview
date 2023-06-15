77ee48ba03ca776b3918950cc3d1c6c79b5e3799
abdou50 <gadourraddadi@gmail.com>
2023-06-15 12:52:57 +0100

    l

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * find_listint_loop - find the loop in a singly linked list
+ * @head: the head of the list
+ * Return: NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+
+	listint_t *slow;
+	listint_t *fast;
+
+	if (head == NULL || head->next == NULL)
+	{
+		return (NULL);
+	}
+	*slow = head->next;
+	*fast = head->next->next;
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (NULL);
+}
