5007072a94a583b3d1bcad3b2ab8b891b0de3f37
abdou50 <gadourraddadi@gmail.com>
2023-08-03 23:46:49 +0100

    l

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,7 +1,12 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* INCLUDED LIBRARIES */
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+
+/* STRUCTS AND DEFINITIONS */
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,9 +22,13 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
+void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
+int is_bst_recursive(const binary_tree_t *tree, int min, int max);
+int is_balanced_preorder(const binary_tree_t *tree);
+int is_balanced(const binary_tree_t *tree);
+size_t height_helper(const binary_tree_t *tree);
 
 #endif
