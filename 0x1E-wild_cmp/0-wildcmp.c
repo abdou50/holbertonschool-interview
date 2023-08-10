8f287ee5f3d6b12e0ba5e57b18de3b6f0855e3b8
abdou50 <gadourraddadi@gmail.com>
2023-08-10 23:31:42 +0100

    l

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- * can be considered identical, otherwise return 0.
+ * wildcmp - compares two strings the second string may contain wildcard "*"
  * @s1: first string
  * @s2: second string
- * Return: 1 if identical or 0 otherwise
+ * Return: 1  or 0 
  */
 int wildcmp(char *s1, char *s2)
 {
 	if (s1 == NULL || s2 == NULL)
-        return (0);
-    if (!*s1)
+		return (0);
+	if (!*s1)
 	{
 		if (*s2 == '*')
 			return (wildcmp(s1, s2 + 1));
