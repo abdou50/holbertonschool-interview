db2be6943825a439d119ac572705c6ee8cec173a
abdou50 <gadourraddadi@gmail.com>
2023-08-10 23:28:04 +0100

    l

---
+++ 0x1E-wild_cmp/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    return (0);
+}
