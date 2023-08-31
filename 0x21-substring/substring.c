2a064f88858e0399d6810aa7b640adf0eb9cabac
abdou50 <gadourraddadi@gmail.com>
2023-08-31 23:45:58 +0100

    l

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "substring.h"
+/**
+ *find_substring - function
+ *@s: is the string to scan
+ *@words: is the array of words all substrings must be a concatenation
+ *@nb_words: is the number of elements in the array words
+ *@n: holds the address at which to store the number of elements
+ * Return: 1 or 0
+ **/
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+
+}
